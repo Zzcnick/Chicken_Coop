c0b7781c223a90e0bdb12db0292f35a8f93f9c85
Zzcnick <zzhen@stuy.edu>
2017-01-23 01:23:48 -0500

    work on multiplayer, set up select networking architecture

--- setserver.c
+++ setserver.c
@@ -8,6 +8,8 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+#define STDIN 0
+
 int s_setup() {
   int sd = -1; // Socket Descriptor
 
@@ -16,7 +18,8 @@ int s_setup() {
   
   struct sockaddr_in sin;
   sin.sin_family = AF_INET;
-  sin.sin_addr.s_addr = INADDR_ANY;
+  inet_aton( "127.0.0.1", &(sin.sin_addr) );
+  //sin.sin_addr.s_addr = INADDR_ANY;
   sin.sin_port = htons(5375);
     
   bind( sd, (struct sockaddr *)&sin, sizeof(sin) );
@@ -29,7 +32,7 @@ int s_connect(int sd) {
   if (listen(sd, 1))
     return -1; // Error
   printf("Server listening...\n");
-  
+
   struct sockaddr_in sin;
   unsigned int sin_size = sizeof(sin);
   connection = accept( sd, (struct sockaddr *)&sin, &sin_size );
@@ -42,13 +45,128 @@ int s_connect(int sd) {
 }
 
 int main() {
-  int sd = -1; int fd = -1;
+  char buffer[128]; // Reading From Clients
+  fd_set fds;
+  char * message = "Connected!\n";
+  int max_clients = 4;
+  int max_sd;
+  int i;
+
+  // Initializing Client Sockets
+  //for (i = 0; i < max_clients; i++)
+
+  int sd;
   sd = s_setup();
   if (sd > -1) {
     printf("Socket successfully created!\n");
   }
-  fd = s_connect(sd);
-  if (fd > -1) {
-    printf("Connection established!\n");
+
+  int host = -1;
+  host = s_connect(sd);
+  if (host > -1) {
+    printf("Connection established to host!\n");
   }
+
+  max_sd = host;
+
+  while (1) {
+    FD_ZERO(&fds);
+    FD_SET(STDIN, &fds);
+    FD_SET(host, &fds);
+    printf("Host: %d\n", max_sd);
+    printf("Selecting \n");
+
+    select( max_sd + 1, &fds, NULL, NULL, NULL);
+    
+    if (FD_ISSET(STDIN, &fds)) {
+      printf("Received input from stdin!\n");
+      fgets( buffer, sizeof(buffer), stdin );
+      printf("[SERVER] %s\n", buffer);
+    }
+    if (FD_ISSET(host, &fds)) {
+      printf("Received input from client!\n");
+      recv(host, buffer, sizeof(buffer), 0);
+      send(host, "Hello\n", 7, 0);
+    }
+  }
+  
+  return 0;
 }
+
+
+/*
+int main() {
+  int master_socket;
+  int addrlen;
+  int new_socket;
+  int client_socket[32];
+  int max_clients = 32;
+  int i;
+  int sd, max_sd;
+  int activity;
+  int valread;
+  char buffer[1024];
+
+  fd_set readfds;
+  char * message = "You have connected!";
+
+  for (i = 0; i < max_clients; i++)
+    client_socket[i] = 0;
+
+  master_socket = s_setup();
+  listen(master_socket, 1);
+  
+  printf("Waiting for connections...\n");
+
+  while (1) {
+    printf("Hello\n");
+
+    FD_ZERO(&readfds);
+    FD_SET(master_socket, &readfds);
+    
+    // Adding Child Sockets
+    for (i = 0; i < max_clients; i++) {
+      sd = client_socket[i];
+
+      if (sd > 0) // client connected
+	FD_SET(sd, &readfds);
+
+      if (sd > max_sd) // max_sd used for select
+	max_sd = sd;
+    }
+    
+    activity = select( max_sd + 1,
+		       &readfds,
+		       0, 0, 0 );
+
+    // Check Master Socket
+    if (FD_ISSET(master_socket, &readfds)) {
+      new_socket = s_connect(master_socket);
+      
+      for (i = 0; i < max_clients; i++) {
+	if (client_socket[i] == 0) { // empty space
+	  client_socket[i] = new_socket;
+	  break;
+	}
+      }
+    }
+
+    // Check Other Sockets
+    for (i = 0; i < max_clients; i++) {
+      sd = client_socket[i];
+
+      if (FD_ISSET(sd, &readfds)) {
+	valread = read( sd, buffer, 1023 );
+	if (valread == 0) { // Client Disconnected
+	  close(sd);
+	  client_socket[i] = 0;
+	} else {
+	  buffer[valread] = 0;
+	  printf("[SERVER] Read %s\n", buffer);
+	}
+      }
+    }
+  }
+  return 0;
+}
+*/
